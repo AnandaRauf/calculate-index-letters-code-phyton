503e718d829a1ff455984720486e4787fa73c9bd
AnandaRauf <uzumaki.nanda@rocketmail.com>
2018-05-10 20:31:42 +0700

    versi 1.0.0

---
+++ indexhuruf.py
@@ -0,0 +1,19 @@
+"""
+Author : Ananda Rauf Maududi
+10 Mei 2018
+Code Phyton The Best
+Example :
+The string "PYTHON" has six characters,
+numbered 0 to 5, as shown below:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+So if you wanted "Y", you could just type
+"PYTHON"[1] (always start counting from 0!)
+"""
+Kata = "Rauf Love Rhaina"[0]
+
+print Kata
